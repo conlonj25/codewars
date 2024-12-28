8ffc2ed1a2d7634345dca3465b224e2102266158
James Conlon <conlonj25@gmail.com>
2024-12-28 16:44:24 +0000

    add vitest and greed-is-good

---
+++ src/greed-is-good/index.ts
@@ -0,0 +1,14 @@
+export const score = (dice: number[]): number => {
+  const nums: number[] = [0, 0, 0, 0, 0, 0, 0];
+  const tripWeights: number[] = [0, 1000, 200, 300, 400, 500, 600];
+  const singleWeights: number[] = [0, 100, 0, 0, 0, 50, 0];
+
+  dice.forEach((x) => (nums[x] = nums[x] + 1));
+  const trips = nums.map((x) => Math.floor(x / 3));
+  const singles = nums.map((x) => x % 3);
+
+  return (
+    trips.reduce((acc, cv, i) => acc + cv * tripWeights[i], 0) +
+    singles.reduce((acc, cv, i) => acc + cv * singleWeights[i], 0)
+  );
+};
